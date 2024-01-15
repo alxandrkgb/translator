2b90b3c7cee056ae7663676d94c3abfda262df6c
Gyekye Alexander <gyekyealexander1@gmail.com>
2024-01-15 05:30:37 -0600

    Update dependencies and add Flowbite plugin

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,246 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "flowbite";
 
 export default function Home() {
+  const [languages, setLanguages] = useState([]);
+
+  const [text, setText] = useState('');
+  const [charCount, setCharCount] = useState(0);
+  const [detectedLanguage, setDetectedLanguage] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState('es');
+  const [languageDict, setLanguageDict] = useState({});
+
+  useEffect(() => {
+    const fetchLanguages = async () => {
+      try {
+        const response = await axios.get('https://libretranslate.de/languages');
+        const dict = {};
+        setLanguages(response.data);
+        response.data.forEach((language) => {
+          dict[language.code] = language.name;
+        });
+        setLanguageDict(dict);
+      } catch (error) {
+        console.error('Failed to fetch languages', error);
+      }
+    };
+
+    fetchLanguages();
+  }, []);
+
+  const handleTextChange = async (event) => {
+    const text = event.target.value;
+    setText(text);
+    setCharCount(text.length);
+
+    if (text) {
+      try {
+        const response = await axios.post('https://libretranslate.de/detect', { q: text });
+        if (response.data && response.data.length > 0) {
+          setDetectedLanguage(response.data[0].language);
+          setSelectedLanguage(response.data[0].language);
+        }
+      } catch (error) {
+        console.error('Failed to detect language', error);
+      }
+    } else {
+      setDetectedLanguage('');
+      setSelectedLanguage('es');
+    }
+  };
+
+  const handleLanguageSelect = (languageCode) => {
+    setSelectedLanguage(languageCode);
+  };
+
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div>
+        <img className="h-[50vh] w-[100%]" src="../hero_img.jpg"></img>
+      </div>
+
+      <div className="flex justify-center -mt-[250px]">
+        <img src="../assets/logo.svg"></img>
+      </div>
+
+      {/* Detected Language */}
+
+      <div className="flex flex-row justify-center gap-6 mt-[80px]">
+        <div className="w-[40%] h-[55vh] bg-[#212936cc] border rounded-xl border-[#4D5562] p-5">
+          <div className="pb-3 flex flex-row gap-4">
+            <p className="font-bold text-[#4D5562] p-1">Detect Language</p>
+            <button className={`rounded-lg font-medium text-[#4D5562] ${detectedLanguage === 'en' ? 'active' : ''}`}>
+              English
+            </button>
+            <button className={`rounded-lg font-medium text-[#4D5562] ${detectedLanguage === 'fr' ? 'active' : ''}`}>
+              French
+            </button>
+            <button
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              className={`text-[#4D5562] rounded-lg text-base px-5 py-2.5 text-center inline-flex items-center ${selectedLanguage || selectedLanguage === detectedLanguage ? 'active' : ''}`}
+            >{languageDict[selectedLanguage]}
+              <svg
+                class="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            <div
+              id="dropdown"
+              class={`z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 `}
+            >
+              <ul
+                class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                {languages.map((language) => (
+                <li key={language.code} value={language.code} selected={language.code === selectedLanguage}>
+                  <button
+                    onClick={() => handleLanguageSelect(language.code)}
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >{language.name}</button>
+                </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <hr className="border-[#394150]"></hr>
+
+          <div className="mt-5">
+            <textarea
+              className="resize-none text-base text-justify text-wrap text-[#F9FAFB] font-medium p-2 bg-transparent w-[36.5vw] h-[24vh]"
+              onChange={handleTextChange}
+          maxLength={500}
+            ></textarea>
+          </div>
+
+          <p className="text-[#4D5562] text-sm text-right">{charCount}/500</p>
+
+          <div className="flex flex-row items-center pt-2 gap-2">
+            <div>
+              <button className="w-[30px] p-1 border-2 border-[#4D5562] rounded-lg">
+                <img src="../assets/sound_max_fill.svg"></img>
+              </button>
+            </div>
+            <div>
+              <button className="w-[30px] p-1 border-2 border-[#4D5562] rounded-lg">
+                <img src="../assets/Copy.svg"></img>
+              </button>
+            </div>
+            <div className="ml-[14rem] text-center text-base font-bold text-[#F9FAFB]">
+              <button className="w-[150px] h-[40px] border-1 rounded-lg border-[#CDD5E0] bg-[#3662E3]">
+                <img
+                  className="inline-block"
+                  src="../assets/Sort_alfa.svg"
+                ></img>
+                Translate
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* End of Detected Language */}
+
+        {/* Target Language */}
+
+        <div className="w-[40%] h-[55vh] bg-[#121826cc] border rounded-xl border-[#4D5562] p-5">
+          <div className="pb-3 flex flex-row gap-4">
+            <button className={`rounded-lg font-medium text-[#4D5562] `}>
+              English
+            </button>
+            <button className={`rounded-lg font-medium text-[#4D5562] `}>
+              French
+            </button>
+            <button
+              id="dropdownButton"
+              data-dropdown-toggle="dropdown2"
+              className={`text-[#4D5562] rounded-lg text-base px-5 py-2.5 text-center inline-flex items-center `}
+              type="button"
+            >
+              <svg
+                class="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            <div
+              id="dropdown2"
+              class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+            >
+              <ul
+                class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownButton"
+              >
+                {languages.map((language) => (
+                <li key={language.code} value={language.code}>
+                  <a
+                    href="#"
+                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >{language.name}</a>
+                </li>
+                ))}
+              </ul>
+            </div>
+
+            <div>
+              <button className="ml-[8rem] w-[30px] p-1 border-2 border-[#4D5562] rounded-lg">
+                <img src="..\assets\Horizontal_top_left_main.svg"></img>
+              </button>
+            </div>
+          </div>
+
+          <hr className="border-[#394150]"></hr>
+
+          <div className="mt-5">
+            <textarea
+              className="disabled resize-none text-base text-justify text-wrap text-[#F9FAFB] font-medium p-2 bg-transparent w-[36.5vw] h-[24vh]"
+              disabled
+              value=""
+            ></textarea>
+          </div>
+
+          <div className="flex flex-row items-center pt-8 gap-2">
+            <div>
+              <button className="w-[30px] p-1 border-2 border-[#4D5562] rounded-lg">
+                <img src="../assets/sound_max_fill.svg"></img>
+              </button>
+            </div>
+            <div>
+              <button className="w-[30px] p-1 border-2 border-[#4D5562] rounded-lg">
+                <img src="../assets/Copy.svg"></img>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
